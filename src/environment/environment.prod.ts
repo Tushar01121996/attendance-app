6c490ed70d404587146abd49fecd8a394898f424
Tushar <tv11266@gmail.com>
2026-07-19 18:50:11 +0530

    changes in url

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment ={
     production: true,
-     apiUrl : 'https://practicewebapi.onrender.com//api',
+     apiUrl : 'https://practicewebapi.onrender.com/api',
      domainUrl : 'https://practicewebapi.onrender.com'
     // apiUrl : 'http://stuattendanceapi.eu-north-1.elasticbeanstalk.com/api',
     // domainUrl : 'http://stuattendanceapi.eu-north-1.elasticbeanstalk.com',
--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,7 +1,7 @@
 export const environment ={
     production: false,
-  domainUrl: 'https://practicewebapi.onrender.com/',
-  apiUrl: 'https://practicewebapi.onrender.com/api/'
+  domainUrl: 'https://practicewebapi.onrender.com',
+  apiUrl: 'https://practicewebapi.onrender.com/api'
     //  apiUrl : 'https://localhost:7261/api',
     //  domainUrl : 'https://localhost:7261'
     // apiUrl : 'http://stuattendanceapi.eu-north-1.elasticbeanstalk.com/api',
