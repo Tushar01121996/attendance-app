0b6ea74d3918d08af237ee54fb2635f2bfc69210
Tushar <tv11266@gmail.com>
2026-07-20 10:27:08 +0530

    update route.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,66 +1,62 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
 import { authGuard } from './auth/auth.guard';
-import { StudentRegistrationComponent } from './students/student-registration/student-registration.component';
-import { StudentListComponent } from './students/student-list/student-list.component';
-import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
-import { StudentAttendanceComponent } from './students/student-attendance/student-attendance.component';
-import { ForgotpasswordComponent } from './auth/forgotpassword/forgotpassword.component';
-import { ResetpasswordComponent } from './auth/resetpassword/resetpassword.component';
-import { NewdashboardComponent } from './newdashboard/newdashboard.component';
-import { HolidaylistComponent } from './Holidays/holidaylist/holidaylist.component';
-import { StudentProfileComponent } from './students/student-profile/student-profile.component';
-import { StudentAttendanceHistoryComponent } from './students/student-attendance-history/student-attendance-history.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
+
+  {
+    path: 'login',
+    loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent)
+  },
+  {
+    path: 'forgotpassword',
+    loadComponent: () => import('./auth/forgotpassword/forgotpassword.component').then(m => m.ForgotpasswordComponent)
+  },
+  {
+    path: 'resetpassword',
+    loadComponent: () => import('./auth/resetpassword/resetpassword.component').then(m => m.ResetpasswordComponent)
+  },
+
   {
     path: 'dashboard',
-    component: DashboardComponent,
+    loadComponent: () => import('./dashboard/dashboard/dashboard.component').then(m => m.DashboardComponent),
     canActivate: [authGuard]
   },
   {
     path: 'newdashboard',
-    component: NewdashboardComponent,
+    loadComponent: () => import('./newdashboard/newdashboard.component').then(m => m.NewdashboardComponent),
+    canActivate: [authGuard]
+  },
+
+  {
+    path: 'students/register',
+    loadComponent: () => import('./students/student-registration/student-registration.component').then(m => m.StudentRegistrationComponent),
     canActivate: [authGuard]
   },
   {
-    path: 'forgotpassword',
-    component: ForgotpasswordComponent
-  },
-   {
-    path: 'resetpassword',
-    component: ResetpasswordComponent
-  },
-  {
-    path: 'students/register',
-    component: StudentRegistrationComponent,
-    canActivate: [authGuard]   // ✅ Protect students page
-  },
-  {
     path: 'students/list',
-    component: StudentListComponent,
+    loadComponent: () => import('./students/student-list/student-list.component').then(m => m.StudentListComponent),
     canActivate: [authGuard]
   },
   {
     path: 'students/attendance',
-    component: StudentAttendanceComponent,
+    loadComponent: () => import('./students/student-attendance/student-attendance.component').then(m => m.StudentAttendanceComponent),
     canActivate: [authGuard]
   },
   {
     path: 'students/:id',
-    component: StudentProfileComponent,
-    canActivate: [authGuard]
-  },
-    {
-    path: 'students/:id/attendance-history',
-    component: StudentAttendanceHistoryComponent,
+    loadComponent: () => import('./students/student-profile/student-profile.component').then(m => m.StudentProfileComponent),
     canActivate: [authGuard]
   },
   {
+    path: 'students/:id/attendance-history',
+    loadComponent: () => import('./students/student-attendance-history/student-attendance-history.component').then(m => m.StudentAttendanceHistoryComponent),
+    canActivate: [authGuard]
+  },
+
+  {
     path: 'holidaylist',
-    component: HolidaylistComponent,
+    loadComponent: () => import('./Holidays/holidaylist/holidaylist.component').then(m => m.HolidaylistComponent),
     canActivate: [authGuard]
   },
 
